1d04df001651b56da3c14741b63a0a0f50715507
Sidh Jain <sidhj211@gmail.com>
2024-03-17 22:40:16 +0530

    back to normal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,13 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Admin from "./components/Admin";
-
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </div>
+    <>
+      <Admin />
+    </>
   );
 }
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+export default App;
