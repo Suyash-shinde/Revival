74d4882dfb10d660490613640c8dcb175b11ce11
Sidh Jain <sidhj211@gmail.com>
2024-03-17 22:39:20 +0530

    stashing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Admin from "./components/Admin";
+
 function App() {
   return (
-    <>
-      <Admin />
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </div>
   );
 }
 
-export default App;
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
